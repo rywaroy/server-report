7eb7d3b702722269706dedaa882b7f092d0e02fb
zhangzhihao <464485682@qq.com>
2019-01-09 17:56:30 +0800

    add

--- app/controller/error.js
+++
@@ -1,9 +0,0 @@
-const Controller = require('egg').Controller;
-
-class ErrorController extends Controller {
-  async index() {
-    this.ctx.body = 'Hello world';
-  }
-}
-
-module.exports = ErrorController;
--- app/router.js
+++ app/router.js
@@ -1,5 +1,5 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
-  router.get('/error', controller.error.index);
+  router.post('/users', controller.users.create);
 };
