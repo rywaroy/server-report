fe50e838dc87cf5c5087aab6f0b8936dedb5e08e
zhangzhihao <464485682@qq.com>
2019-01-11 17:34:54 +0800

    Add getlist api

--- app/controller/errors.js
+++ app/controller/errors.js
@@ -1,19 +1,48 @@
 const Controller = require('egg').Controller;
+const dayjs = require('dayjs');
 
 class ErrorsController extends Controller {
 
   async create() {
     const ctx = this.ctx;
     const { project, msg, url, line, col } = ctx.request.body;
-    const errors = await ctx.model.Errors.create({
-      project,
-      msg,
-      url,
-      line,
-      col,
+    await ctx.model.Errors.findOrCreate({
+      where: {
+        project,
+        line,
+        col,
+        msg,
+        url,
+      },
+    })
+    ctx.status = 200;
+    ctx.body = {
+      msg: '添加成功'
+    };
+  }
+
+  async list() {
+    const ctx = this.ctx;
+    let { page, limit, project, date } = ctx.query;
+    page = Number(page) || 1;
+    limit = Number(limit) || 10;
+    const data = await ctx.model.Errors.findAndCount({
+      where: {
+        ...(project && { project }),
+        ...(date && { created_at: {
+          $lt: new Date(dayjs(date).valueOf() + 24 * 60 * 60 * 1000),
+          $gt: new Date(date),
+        } }),
+      },
+      order: [['id', 'desc']],
+      limit,
+      offset: (page - 1) * limit,
     });
     ctx.status = 200;
-    ctx.body = errors;
+    ctx.body = {
+      list: data.rows,
+      total: data.count
+    };
   }
 }
 
