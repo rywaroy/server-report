81f900a456f03b8b57ea0518fe869e8907c61c04
zhangzhihao <464485682@qq.com>
2019-01-11 17:35:18 +0800

    Add

--- app/router.js
+++ app/router.js
@@ -2,4 +2,5 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
   router.post('/errors', controller.errors.create);
+  router.get('/errors', controller.errors.list);
 };
