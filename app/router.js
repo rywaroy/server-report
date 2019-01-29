e22474cc08233ad9ba3f163da749ca43280e7985
zhangzhihao <464485682@qq.com>
2019-01-29 16:46:23 +0800

    add: jwt

--- app/router.js
+++ app/router.js
@@ -1,11 +1,12 @@
 module.exports = app => {
-  const { router, controller } = app;
+  const { router, controller, jwt } = app;
   router.get('/', controller.home.index);
   router.post('/errors', controller.errors.create);
-  router.get('/errors', controller.errors.list);
-  router.post('/fix/:id', controller.errors.fix);
-  router.get('/location', controller.errors.location);
+  router.get('/errors', jwt, controller.errors.list);
+  router.post('/fix/:id', jwt, controller.errors.fix);
+  router.get('/location', jwt, controller.errors.location);
   router.post('/performance', controller.performance.create);
-  router.get('/projects', controller.projects.list);
-  router.post('/projects', controller.projects.create);
+  router.get('/projects', jwt, controller.projects.list);
+  router.post('/projects', jwt, controller.projects.create);
+  router.post('/login', controller.users.login);
 };
